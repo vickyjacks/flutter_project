66a2ce79e7a16def010f2c56f0abc899b173b848
Ankit Yadav <ankitky778@gmail.com>
2022-05-08 05:22:11 -0700

    project setup

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -69,7 +69,7 @@ class _LoginPageState extends State<LoginPage> {
                           changeButton = true;
                         });
                         await Future.delayed(Duration(seconds: 1));
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        await Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
                       child: AnimatedContainer(
                         duration: Duration(seconds: 1),
